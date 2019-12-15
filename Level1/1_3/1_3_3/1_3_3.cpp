569a85414de83103985823dfd35216a8ef9ae623
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-14 21:29:45 -0600

    update 1.3.3

--- Level1/1_3/1_3_3/1_3_3.cpp
+++ Level1/1_3/1_3_3/1_3_3.cpp
@@ -12,16 +12,20 @@ int main()
     int x;
     
     x=-3+4*5-6;
-    printf("x=%d\n", x); // This line will print 11
+    printf("x=%d\n", x); // This line will print 11, 
+    // the equation is equivalent to -3 + (4 * 5) - 6
     
     x=3+4%5-6;
     printf("x=%d\n", x); // This line will print 1
+    // the equation is equivalent to 3 + (4 % 5) - 6
     
     x=-3*4%-6/5;
     printf("x=%d\n", x); // This line will print 0
+    // the formula is equivalent to ((-3 * 4) % (-6)) / 5
     
     x=(7+6)%5/2;
     printf("x=%d\n", x); // This line will print 1
+    // the formula is equivalent to 13 % 5 / 2 = 3 / 2 = 1
     
     return 0;
 }
