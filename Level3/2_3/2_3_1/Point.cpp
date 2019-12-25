bcb88c056f63e8faa41eccea7b94e1fc22e350ce
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-25 10:40:36 -0800

    add 2_3_1

---
+++ Level3/2_3/2_3_1/Point.cpp
@@ -0,0 +1,60 @@
+/*  Point.cpp
+ *  ------------------------------------------
+ *  @description: source file of Point class
+ *  @author: Jiayuan Li
+ *  @version: 1.0 Dec 25, 2019
+ */
+
+#include "Point.hpp"
+#include <string.h>
+#include <cmath>
+
+using std::to_string;
+using std::sqrt;
+
+Point::Point(): m_x(0), m_y(0)
+{
+    // default constructor
+}
+
+Point::~Point()
+{
+    // default destructor
+}
+
+double Point::GetX()
+{
+    return m_x;
+}
+
+double Point::GetY()
+{
+    return m_y;
+}
+
+void Point::SetX(double new_x)
+{
+    m_x = new_x;
+}
+
+void Point::SetY(double new_y)
+{
+    m_y = new_y;
+}
+
+string Point::ToString()
+{
+    return "Point(" + to_string(m_x) + ", " + to_string(m_y) + ")";
+}
+
+double Point::DistanceOrigin()
+{
+    return sqrt(m_x * m_x + m_y * m_y);
+}
+
+double Point::Distance(Point p)
+{
+    double x_rel = m_x - p.m_x; // relative distance of x
+    double y_rel = m_y - p.m_y; // relative distance of y
+    return sqrt(y_rel * y_rel + x_rel * x_rel);
+}
