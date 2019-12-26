56b9924eb64b53006f085390febcea267124b46f
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-25 22:54:20 -0800

    2_3_5 submission

---
+++ Level3/2_3/2_3_5/Line.hpp
@@ -0,0 +1,41 @@
+/*  Line.hpp
+ *  ------------------------------------------
+ *  @description: header file of Line class
+ *  @author: Jiayuan Li
+ *  @version: 1.0 Dec 25, 2019
+ */
+
+#ifndef LINE_HPP
+#define LINE_HPP
+
+#include <iostream>
+#include "Point.hpp"
+
+using std::string;
+
+class Line
+{
+public:
+    // constructors and destructor
+    Line();                         // default constructor
+    Line(const Point& start, const Point & end);    // constructor with two point
+    Line(const Line& other);        // copy constructor
+    ~Line();                        // destructor
+
+    // getter and setter functions
+    Point P1() const;               // getter function for start point
+    Point P2() const;               // getter function for end point
+    void P1(const Point& other);    // setter function for start point
+    void P2(const Point& other);    // setter function for end point
+
+    // functions return the properties of the line
+    string ToString() const;        // returns description of the line
+    double Length() const;          // returns the length of the line
+
+private:
+    // start point and end point
+    Point p_start;
+    Point p_end;
+};
+
+#endif
