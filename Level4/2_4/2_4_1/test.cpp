87cc4c000dc395748e0e9512bfa9aa328ff01dc3
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-27 11:18:45 -0800

    file submission

---
+++ Level4/2_4/2_4_1/test.cpp
@@ -0,0 +1,33 @@
+/*  test.cpp
+ *  ------------------------------------------
+ *  @description: test file for class Point
+ *  @author: Jiayuan Li
+ *  @version: 1.0 Dec 25, 2019
+ */
+
+#include <iostream>
+#include "Point.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+    Point test_point_1 = Point(1, 1);
+    Point test_point_2 = Point(0, 0);
+
+    cout << "point a: " << test_point_1.ToString() << endl;
+    cout << "point b: " << test_point_2.ToString() << endl;
+    cout << "test case 1 -- -a    : " << (- test_point_1).ToString() << endl;
+    cout << "test case 2 -- a * 2 : " << (test_point_1 * 2.).ToString() << endl;
+    cout << "test case 3 -- a + b : " << (test_point_1 + test_point_2).ToString() << endl;
+    cout << "test case 4 -- a == b: " << ((test_point_1 == test_point_2) ? "true" : "false") << endl;
+    cout << "test case 5 -- a == a: " << ((test_point_1 == test_point_1) ? "true" : "false") << endl;
+    test_point_2 = test_point_1;
+    cout << "test case 6 -- b = a : " << test_point_2.ToString() << endl;
+    cout << "test case 7 -- a *= 2: " << (test_point_1 *= 2).ToString() << endl;
+
+    return 0;
+}
