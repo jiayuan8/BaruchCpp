1944587489966f4489938c56df74295bc06928aa
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-27 22:34:28 -0800

    add files

---
+++ Level4/2_5/2_5_1/test.cpp
@@ -0,0 +1,28 @@
+/*  test.cpp
+ *  ------------------------------------------
+ *  @description: test file for class Point
+ *  @author: Jiayuan Li
+ *  @version: 1.0 Dec 25, 2019
+ */
+
+#include <iostream>
+#include "Point.hpp"
+#include "Line.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+    Point test_point_1 = Point(1, 1);
+    cout << "----------------test for Point----------------\n";
+    cout << "point a: " << test_point_1 << endl;
+
+    Line test_line_2 = Line(Point(2.33, 2.33), Point(6.66, 6.66));
+    cout << "----------------test for Line----------------\n";
+    cout << "line a: " << test_line_2 << endl;
+
+    return 0;
+}
