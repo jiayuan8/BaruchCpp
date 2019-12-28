3702d93ef3e10991b99a014ae0be9455a0964514
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-28 09:52:50 -0800

    submission

--- Level4/2_6/2_6_1/Array.cpp
+++ Level4/2_6/2_6_1/Array.cpp
@@ -10,95 +10,102 @@
 using std::cout;
 using std::endl;
 
-Array::Array(): m_data(new Point[10]), arr_size(10)
+namespace JIAYUAN
 {
-    // default constructor, the arr size will be 10
-}
-
-Array::Array(size_t size)
-: m_data(new Point[size >= 1 ? size : 1]), arr_size(size >= 1 ? size : 1)
-{
-    // constructor with size parameter, the size need to be at least 1
-    // will be cast to 1 if a 0 is given    
-}
-
-Array::Array(const Array& other): m_data(new Point[other.Size()]), arr_size(other.Size())
-{
-    // copy constructor
-    for (size_t i = 0; i < other.Size(); i++)
+    namespace Containers
     {
-        m_data[i] = other[i];
+        Array::Array(): m_data(new CAD::Point[10]), arr_size(10)
+        {
+            // default constructor, the arr size will be 10
+        }
+
+        Array::Array(size_t size)
+        : m_data(new CAD::Point[size >= 1 ? size : 1]), arr_size(size >= 1 ? size : 1)
+        {
+            // constructor with size parameter, the size need to be at least 1
+            // will be cast to 1 if a 0 is given    
+        }
+
+        Array::Array(const Array& other): m_data(new CAD::Point[other.Size()]), arr_size(other.Size())
+        {
+            // copy constructor
+            for (size_t i = 0; i < other.Size(); i++)
+            {
+                m_data[i] = other[i];
+            }
+        }
+
+        Array::~Array()
+        {
+            // destructor
+            delete[] m_data;
+        }
+
+        size_t Array::Size() const
+        {
+            return arr_size;
+        }
+
+        CAD::Point& Array::operator [] (int index)
+        {
+            if (index >= arr_size)
+            {
+                return m_data[0];
+            }
+            return m_data[index];
+        }
+
+        const CAD::Point& Array::operator [] (int index) const
+        {
+            // this allows us to call for [] for a const object in a const function
+            // read only, cannot write
+            if (index >= arr_size)
+            {
+                return m_data[0];
+            }
+            return m_data[index];
+        }
+
+        Array& Array::operator = (const Array& source)
+        {
+            // preclude assigning to itself
+            if (this == &source)
+            {
+                return *this;
+            }
+
+            // free the original memory
+            delete [] m_data;
+
+            // copy the elements
+            arr_size = source.Size();
+            m_data = new CAD::Point[arr_size];
+            for (size_t i = 0; i < arr_size; i++)
+            {
+                m_data[i] = source[i];
+            }
+            return *this;
+        }
+
+        void Array::SetElement(size_t index, const CAD::Point& element) const
+        {
+            // check if the index is out of bound
+            if (index >= arr_size)
+            {
+                return;
+            }
+            m_data[index] = element;
+        }
+
+        const CAD::Point& Array::GetElement(size_t index) const
+        {
+            // if the index is out of bound, return the first element
+            if (index >= arr_size)
+            {
+                return m_data[0];
+            }
+            return m_data[index];
+        }
     }
 }
-
-Array::~Array()
-{
-    // destructor
-    delete[] m_data;
-}
-
-size_t Array::Size() const
-{
-    return arr_size;
-}
-
-Point& Array::operator [] (int index)
-{
-    if (index >= arr_size)
-    {
-        return m_data[0];
-    }
-    return m_data[index];
-}
-
-const Point& Array::operator [] (int index) const
-{
-    // this allows us to call for [] for a const object in a const function
-    // read only, cannot write
-    if (index >= arr_size)
-    {
-        return m_data[0];
-    }
-    return m_data[index];
-}
-
-Array& Array::operator = (const Array& source)
-{
-    // preclude assigning to itself
-    if (this == &source)
-    {
-        return *this;
-    }
-
-    // free the original memory
-    delete [] m_data;
-
-    // copy the elements
-    arr_size = source.Size();
-    m_data = new Point[arr_size];
-    for (size_t i = 0; i < arr_size; i++)
-    {
-        m_data[i] = source[i];
-    }
-    return *this;
-}
-
-void Array::SetElement(size_t index, const Point& element) const
-{
-    // check if the index is out of bound
-    if (index >= arr_size)
-    {
-        return;
-    }
-    m_data[index] = element;
-}
-
-const Point& Array::GetElement(size_t index) const
-{
-    // if the index is out of bound, return the first element
-    if (index >= arr_size)
-    {
-        return m_data[0];
-    }
-    return m_data[index];
-}
+        
--- Level4/2_6/2_6_1/Array.hpp
+++ Level4/2_6/2_6_1/Array.hpp
@@ -11,25 +11,31 @@
 #include <iostream>
 #include "Point.hpp"
 
-class Array 
+namespace JIAYUAN
 {
-public:
-    Array();                // default constructor
-    Array(size_t size);     // constructor with size argument
-    Array(const Array& other);  // copy constructor
-    ~Array();               // destructor
+    namespace Containers
+    {
+        class Array 
+        {
+        public:
+            Array();                // default constructor
+            Array(size_t size);     // constructor with size argument
+            Array(const Array& other);  // copy constructor
+            ~Array();               // destructor
 
-    Point& operator [] (int index);                 // overloading assign operator, allow write
-    const Point& operator [] (int index) const;     // overloding the [] operator
-    Array& operator = (const Array& source);        // overloading assign operator
+            CAD::Point& operator [] (int index);                 // overloading assign operator, allow write
+            const CAD::Point& operator [] (int index) const;     // overloding the [] operator
+            Array& operator = (const Array& source);        // overloading assign operator
 
-    size_t Size() const;    // return the size of the arr
-    void SetElement(size_t index, const Point& element) const;  // set the element of the array
-    const Point& GetElement(size_t index) const;    // return the element of the array given the index
-     
-private:
-    Point* m_data;          // pointer pointing to the data
-    size_t arr_size;        // size of array
-};
+            size_t Size() const;    // return the size of the arr
+            void SetElement(size_t index, const CAD::Point& element) const;  // set the element of the array
+            const CAD::Point& GetElement(size_t index) const;    // return the element of the array given the index
+            
+        private:
+            CAD::Point* m_data;          // pointer pointing to the data
+            size_t arr_size;        // size of array
+        };
+    }
+}
 
 #endif
