178ddbcadf71e18fae834fd5d5351647c2cbdd0c
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-30 11:29:13 -0800

    start 3_6_1

---
+++ Level5/3_6/3_6_1/test.cpp
@@ -0,0 +1,22 @@
+/*  test.cpp
+ *  ------------------------------------------
+ *  @description: test file for class Point
+ *  @author: Jiayuan Li
+ *  @version: 3.4.1 Dec 29, 2019
+ */
+
+#include <iostream>
+#include "Array.hpp"
+#include "Point.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using namespace JIAYUAN::CAD;
+
+int main()
+{
+
+
+    return 0;
+}
