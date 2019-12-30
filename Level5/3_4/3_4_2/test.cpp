f2a69c6e868d5f055a271cd7e89f960492666ba0
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-29 22:12:07 -0800

    file submission

--- Level5/3_4/3_4_2/test.cpp
+++ Level5/3_4/3_4_2/test.cpp
@@ -17,21 +17,37 @@ using namespace JIAYUAN::CAD;
 
 int main()
 {
-    Line l;
-    /* before implemeting colon syntax:
-     * 4 constructor calls for Point class
-     *      (2 default constructor, 2 constructor by parameters)
-     * 2 assignment calls for Point class
-     * 4 destructor calls for Point class
-     */
+    Shape s;
+    Point p(10, 20);
+    Line l(Point(1,2), Point(3, 4));
 
-    /* after implemeting colon syntax:
-     * 2 constructor calls for Point class
-     * 0 assignment calls for Point class
-     * 2 destructor calls for Point class
-     */
+    cout<<s.ToString()<<endl; 
+    cout<<p.ToString()<<endl; 
+    cout<<l.ToString()<<endl;
 
-    /* Conclusion: the calls are less than before */
+    cout<<"Shape ID: "<<s.ID()<<endl; 
+    cout<<"Point ID: "<<p.ID()<<endl; 
+    /* Q: Does this work? */
+    /* A: Yes, this works.*/
+    cout<<"Line ID: "<<l.ID()<<endl;
+    /* Q: Does this work? */
+    /* A: Yes, this works.*/
+
+    Shape* sp;
+    sp=&p; 
+    /* Q: Point in a shape variable. Possible? */
+    /* A: Yes, it is possible. It compiles with no error. */
+    cout<<sp->ToString()<<endl;
+    /* Q: What is printed? */
+    /* A: "ID: 282475249" is printed. It calls the ToString() in Shape class */
+
+    Point p2;
+    p2=p;
+    cout<<p2<<", "<<p2.ID()<<endl;
+    /* Q: Is the ID copied if you do not call the base class assignment in point? */
+    /* A: Yes, since the assignment operator of Point class (derived) will call the 
+     * assign operator in Shape class (base)
+     */
 
     return 0;
 }
