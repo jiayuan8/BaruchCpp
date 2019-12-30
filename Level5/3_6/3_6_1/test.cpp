8c5c509bbaef2511bca4751c319accecaeafed28
Jiayuan Li <jiayuan8@illinois.edu>
2019-12-30 11:38:18 -0800

    3_6_1 finished

--- Level5/3_6/3_6_1/test.cpp
+++ Level5/3_6/3_6_1/test.cpp
@@ -13,10 +13,52 @@ using std::cout;
 using std::cin;
 using std::endl;
 using namespace JIAYUAN::CAD;
+using namespace JIAYUAN::Containers;
 
 int main()
 {
+    // create an array
+    Array test_array(3);
 
+    // test for operator []
+    try
+    {
+        cout << test_array[3] << endl;
+    }
+    catch(int err_code)
+    {
+        if (err_code == -1)
+        {
+            cout << "Array out of bound error!\n";
+        }
+    }
+
+    // test for getelement
+    try
+    {
+        cout << test_array.GetElement(-1) << endl;
+    }
+    catch(int err_code)
+    {
+        if (err_code == -1)
+        {
+            cout << "Array out of bound error!\n";
+        }
+    }
+
+    // test for set element
+    try
+    {
+        test_array[-1] = Point(8, 8);
+    }
+    catch(int err_code)
+    {
+        if (err_code == -1)
+        {
+            cout << "Array out of bound error!\n";
+        }
+    }
+    
 
     return 0;
 }
