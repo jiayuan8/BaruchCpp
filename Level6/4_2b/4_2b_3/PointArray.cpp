b4c909676308c52779ee8ea000ea097b5723a64d
Jiayuan Li <jiayuan8@illinois.edu>
2020-01-07 15:26:44 -0800

    4_2b_3 completed

--- Level6/4_2b/4_2b_3/PointArray.cpp
+++ Level6/4_2b/4_2b_3/PointArray.cpp
@@ -5,3 +5,55 @@
  *  @version: 1.0 Dec 28, 2019
  */
 
+#include "PointArray.hpp"
+
+namespace JIAYUAN
+{
+    namespace Containers
+    {
+        // constructors
+        PointArray::PointArray(): Array<CAD::Point>()
+        {
+            // default constructor
+        }
+        
+        PointArray::PointArray(size_t size): Array<CAD::Point>(size)
+        {
+            // constructor by parameters
+        }
+
+        PointArray::PointArray(const PointArray& other): Array<CAD::Point>(other)
+        {
+            //copy constructor
+        }
+
+        PointArray::~PointArray()
+        {
+
+        }
+
+        PointArray& PointArray::operator = (const PointArray& other)
+        {
+            if (this == &other)
+            {
+                return *this;
+            }
+            Array<CAD::Point>::operator = (other);
+            return *this;
+        }
+
+        double PointArray::Length() const
+        {
+            if (this->Size() == 1)
+            {
+                return 0;
+            }
+            double dist = 0;
+            for(size_t i = 0; i < this->Size() - 1; i++)
+            {
+                dist += (this->GetElement(i)).Distance(this->GetElement(i + 1));
+            }
+            return dist;
+        }
+    }
+}
--- Level6/4_2b/4_2b_3/test.cpp
+++ Level6/4_2b/4_2b_3/test.cpp
@@ -6,7 +6,7 @@
  */
 
 #include <iostream>
-#include "NumericArray.hpp"
+#include "PointArray.hpp"
 #include "Point.hpp"
 #include "ArrayException.hpp"
 
@@ -19,38 +19,25 @@ using namespace JIAYUAN::Exception;
 
 int main()
 {
-    /* 
-     * Answer: The type used as template argument for Numeric Array 
-     * must support "+" and "*" operation internally (dot product), 
-     * as well as "*" operation with numerical factor(such as int, double, etc)
-     * 
-     * we cannot create a numeric array with point since the Point class 
-     * doesn't support "*" operation with two point objects.
-     */
-    NumericArray<double> test_arr_1(10);
-    NumericArray<double> test_arr_2(10);
-    for (int i = 0; i < 10; i++)
-    {
-        test_arr_1[i] = i + 1;
-        test_arr_2[i] = i + 1;
-    }
+    // test constructors
+    PointArray point_arr_0;
+    PointArray point_arr_1(3);
+    PointArray point_arr_2(point_arr_1);
+    point_arr_1[0] = Point(0, 0);
+    point_arr_1[1] = Point(0, 1);
+    point_arr_1[2] = Point(0, 2);
 
-    test_arr_1.PrintArray();
-    NumericArray<double> test_arr_3 = test_arr_1 + test_arr_2;
-    test_arr_3.PrintArray();
-    test_arr_3 = test_arr_2 * 3;
-    test_arr_3.PrintArray();
-    cout << "Dot Product: " << test_arr_1.DotProduct(test_arr_2) << endl;
+    point_arr_1.PrintArray();
+    point_arr_2.PrintArray();
 
-    NumericArray<double> test_arr_4(5);
-    try
-    {
-        test_arr_1.DotProduct(test_arr_4);
-    }
-    catch(ArrayException& ex)
-    {
-        cout << ex.GetMessage() << endl;
-    }
+    // test assign operator
+    point_arr_2 = point_arr_1;
+
+    point_arr_1.PrintArray();
+    point_arr_2.PrintArray();
+
+    // test Length()
+    cout << "Length: " << point_arr_1.Length() << endl;
 
     return 0;
 }
