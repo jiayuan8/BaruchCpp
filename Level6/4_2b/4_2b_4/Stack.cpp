17b5fc975b68a1f1c4896357ef662e0e115c4c3f
Jiayuan Li <jiayuan8@illinois.edu>
2020-01-07 15:39:13 -0800

    file submission

---
+++ Level6/4_2b/4_2b_4/Stack.cpp
@@ -0,0 +1,6 @@
+/*  Stack.cpp
+ *  ------------------------------------------
+ *  @description: source file of Array class
+ *  @author: Jiayuan Li
+ *  @version: 1.0 Dec 28, 2019
+ */
---
+++ Level6/4_2b/4_2b_4/Stack.hpp
@@ -0,0 +1,6 @@
+/*  Stack.hpp
+ *  ------------------------------------------
+ *  @description: header file of Array class
+ *  @author: Jiayuan Li
+ *  @version: 1.0 Dec 28, 2019
+ */
