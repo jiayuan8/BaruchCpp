3e737b1f3624c34c407f7207786aa30a5eac5592
Jiayuan Li <jiayuan8@illinois.edu>
2020-01-07 16:25:29 -0800

    update stack

--- Level6/4_2b/4_2b_4/Stack.cpp
+++ Level6/4_2b/4_2b_4/Stack.cpp
@@ -4,3 +4,63 @@
  *  @author: Jiayuan Li
  *  @version: 1.0 Dec 28, 2019
  */
+
+#include "Stack.hpp"
+
+namespace JIAYUAN
+{
+    namespace Containers
+    {
+        template <typename T>
+        Stack<T>::Stack(): m_arr(Array<T>()), m_current(0)
+        {
+            // default constructor
+        }
+
+        template <typename T>
+        Stack<T>::Stack(size_t size): m_arr(Array<T>(size)), m_current(0)
+        {
+            // constructor with parameters
+        }
+
+        template <typename T>
+        Stack<T>::Stack(const Stack<T>& other)
+        : m_arr(Array<T>(other.m_arr)), m_current(other.m_current)
+        {
+            // copy constructor
+        }
+
+        template <typename T>
+        Stack<T>::~Stack()
+        {
+            // destructor
+        }
+
+        template <typename T>
+        Stack<T>& Stack<T>::operator = (const Stack<T>& other)
+        {
+            if (this == &other)
+            {
+                return *this;
+            }
+            m_arr = other.m_arr;
+            m_current = other.m_current;
+            return *this;
+        }
+
+        template <typename T>
+        void Stack<T>::Push(const T& new_element)
+        {
+            m_arr[m_current + 1] = new_element;
+            m_current += 1;
+        }
+
+        template <typename T>
+        const T& Stack<T>::Pop()
+        {
+            const T& res = m_arr[m_current - 1];
+            m_current - 1;
+            return res;
+        }
+    }
+}
--- Level6/4_2b/4_2b_4/Stack.hpp
+++ Level6/4_2b/4_2b_4/Stack.hpp
@@ -4,3 +4,41 @@
  *  @author: Jiayuan Li
  *  @version: 1.0 Dec 28, 2019
  */
+
+#ifndef STACK_CPP
+#define STACK_CPP
+
+#include "Array.hpp"
+
+namespace JIAYUAN
+{
+    namespace Containers
+    {
+        template <typename T>
+        class Stack
+        {
+        public:
+            // constructors
+            Stack();                    // default constructor
+            Stack(size_t size);         // constructor with a given size
+            Stack(const Stack<T>& other);  // copy constructor
+
+            // destructor
+            ~Stack();
+
+            // overload assign operator
+            Stack<T>& operator = (const Stack<T>& other);     // overload assign operator
+
+            // other operations
+            void Push(const T& new_element);        // push operation
+            const T& Pop();                         // pop operation
+
+        private:
+            Array<T> m_arr;      // pointer to the array storing data
+            size_t m_current;       // currint index
+        };
+    }
+}
+
+
+#endif
