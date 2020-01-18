4c976e44a4d7947d1fbd5a01504d1db73bd24ab7
Jiayuan Li <jiayuan8@illinois.edu>
2020-01-18 14:33:45 -0600

    submission

---
+++ Level8/8_1/main.cpp
@@ -0,0 +1,55 @@
+/* main.cpp
+ * ----------------------------------------------
+ * @description: test functionality of shared_ptr
+ * @author: Jiayuan Li
+ * @version: 1.0 Jan 18th 2020
+ */
+
+
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+#include "Line.hpp"
+#include "Circle.hpp"
+#include "Point.hpp"
+#include "Array.hpp"
+
+using namespace JIAYUAN::CAD;
+using namespace JIAYUAN::Containers;
+
+typedef boost::shared_ptr<Shape> ShapePtr;
+typedef Array<ShapePtr> ShapeArray;
+
+int main()
+{
+    // create object for test
+    ShapePtr test_point(new Point());
+    ShapePtr test_line(new Line());
+    ShapePtr test_circle(new Circle());
+
+    {
+        // create an array and fill it with the objects
+        ShapeArray test_arr(3);
+        test_arr[0] = test_point;
+        test_arr[1] = test_line;
+        test_arr[2] = test_circle;
+
+        // print the shapes
+        std::cout << test_arr[0]->ToString() << std::endl;
+        std::cout << test_arr[1]->ToString() << std::endl;
+        std::cout << test_arr[2]->ToString() << std::endl;
+
+        // print the shapes
+        std::cout << "point users:  " << test_point.use_count() << std::endl;
+        std::cout << "line users:   " << test_line.use_count() << std::endl;
+        std::cout << "circle users: " << test_circle.use_count() << std::endl;
+
+    }
+
+    // test whether the shared pointer delete object automatically
+    std::cout << "point exists:  " << test_point.use_count() << std::endl;
+    std::cout << "line exists:   " << test_line.use_count() << std::endl;
+    std::cout << "circle exists: " << test_circle.use_count() << std::endl;
+
+    return 0;
+
+}
